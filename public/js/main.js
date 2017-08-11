db8b7a772aa016284f6650cd6b17abbfc456b685
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-08-10 19:13:06 -0500

    portfolio added

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+console.log('hi');
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+      var $a = $(".navbar-default .nav > li > a")
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $a.toggleClass('scrolled', $(this).scrollTop() > $a.height());
+  });
+});
+
+
+
+
+
+$(document).ready(function () {
+    $("nav").find("li").on("click", "a", function () {
+        $('.navbar-collapse.in').collapse('hide');
+    });
+});
+
+ $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top-50
+    }, 500);
+   
+});
